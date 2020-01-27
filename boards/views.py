2b0a6cc33eefa5d199f5a45c83923bca0a5a5543
Alejandro <alexberre97@gmail.com>
2020-01-26 21:15:56 -0800

    Topic post
    Added a topic post view
    Unit test for topic post 
    Added a avatar image but it doesn't correspond to account image and it should be

--- boards/views.py
+++ boards/views.py
@@ -1,13 +1,14 @@
-from django.contrib.auth.models import User
-from django.shortcuts import render, redirect, get_object_or_404	
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+
 from .forms import NewTopicForm
 from .models import Board, Topic, Post
 
-from django.contrib.auth.decorators import login_required
 
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})	
+
 
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
@@ -28,7 +29,13 @@ def new_topic(request, pk):
                 topic=topic,
                 created_by=request.user
             )
-            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+            return redirect('topic_posts', pk=pk, topic_pk=topic.pk)
     else:
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+
+def topic_posts(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    return render(request, 'topic_posts.html', {'topic': topic})
+
