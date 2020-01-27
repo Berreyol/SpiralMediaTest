de29134b1ded1296004c4ebcb5cb228ea55409ed
Alejandro <alexberre97@gmail.com>
2020-01-26 20:32:26 -0800

    User authentication
    Stop user to creating new topics without been logged in

--- boards/views.py
+++ boards/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import NewTopicForm
 from .models import Board, Topic, Post
 
+from django.contrib.auth.decorators import login_required
+
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})	
@@ -11,20 +13,20 @@ def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
 
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    user = User.objects.first()  # TODO: get the currently logged in user
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user
             topic.save()
             post = Post.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=user
+                created_by=request.user
             )
             return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
     else:
